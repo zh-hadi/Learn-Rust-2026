f53eec2a1d389ed98ca63ea8bfda62fd97273d36
Hadiuzzaman <zhhadi50@gmail.com>
2026-02-23 19:04:15 +0600

    string add

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,15 @@
 
 fn main(){
     let s1 = "hello";
-    let s2 = " world";
-    let s3 = format!("{} {}", s1, s2);
+    let s2 = "world";
+    let mut s3 = format!("{} {}", s1, s2);
 
-    println!("{s3}");
+    for c in s3.chars() {
+        println!("{}", c);
+    }
+
+    s3.push('!');
+    s3.push_str(" 2026");
+
+    println!("{}", s3);
 }
