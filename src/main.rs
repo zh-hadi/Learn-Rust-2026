4349d73a4f055b54b7eaee1423a79fe687f04d60
Hadiuzzaman <zhhadi50@gmail.com>
2026-02-23 19:00:38 +0600

    feat: string concatanation using format

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,9 @@
 
-#[derive(Debug)]
-struct User {
-    id: u32,
-    name: String,
-    email: String
-}
 
-impl User {
-    fn greeting(&self){
-        println!("Good Morning {}", self.name);
-    }
-}
+fn main(){
+    let s1 = "hello";
+    let s2 = " world";
+    let s3 = format!("{} {}", s1, s2);
 
-fn main() {
-    let ar = [20, 30, 3, 2, 40];
-    let big_number = big_number_in_array(&ar);
-
-    match big_number {
-        Ok(v) => println!("big number is = {}", v),
-        Err(e) => println!("{e}")
-    }
-
-    println!("{:?}", sort_array(&ar));
-
-    let user1 = User {
-        id: 10,
-        name: "Hadiuzzaman".to_string(),
-        email: "zhhadi50@gmail.com".to_string()
-    };
-
-    println!("{:?}", user1);
-    user1.greeting();
-
-    let name = "hadiuzzaman".to_string();
-
-    for c in name.chars() {
-        println!("{c}");
-    }
-}
-
-
-
-
-fn big_number_in_array(data: &[i32])->Result<i32, String>
-{
-    if data.is_empty(){
-        return Err("Array is emtpy".to_string());
-    }
-
-    let mut big_number = data[0];
-
-    for i in 0..data.len() {
-        if data[i] > big_number {
-            big_number = data[i];
-        }
-    }
-
-    Ok(big_number)
-}
-
-fn sort_array(ar: &[i32])->Result<Vec<i32>, String>
-{
-    let mut vec = ar.to_vec();
-    let len = vec.len();
-    
-    for i in 0..len {
-        for j in 0..len - 1 - i {
-            if vec[j] > vec[j+1] {
-                let temp = vec[j];
-                vec[j] = vec[j+1];
-                vec[j+1] = temp;
-            }
-        }
-    }
-
-    Ok(vec)
+    println!("{s3}");
 }
