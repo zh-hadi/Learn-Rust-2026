491defecea538c814309018cf1f822923ec9057b
Hadiuzzaman <zhhadi50@gmail.com>
2026-03-01 23:31:05 +0600

    input number with string filter

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,11 @@ fn main(){
    
    io::stdin().read_line(&mut input).expect("read line error occors here");
 
-   let data: Vec<i32> = input
+   let data: Vec<i32>= input
                             .trim()
                             .split(",")
                             .map(|x| x.trim())
-                            .map(|x| x.parse::<i32>().unwrap())
+                            .filter_map(|x| x.parse::<i32>().ok())
                             .collect();
 
    println!("{:?}", data);
